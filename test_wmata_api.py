db69e0e1ac7d9544c2486e85eae257cee1a3af29
ypark79 <paul.park86@gmail.com>
2025-10-31 15:05:55 -1000

    Initial Push
    Initial push from local repo to online repo.

--- test_wmata_api.py
+++ test_wmata_api.py
@@ -2,31 +2,46 @@ from wmata_api import app
 import json
 import unittest
 
+
 class WMATATest(unittest.TestCase):
-    # ensure both endpoints return a 200 HTTP code
+    # Ensure both endpoints return a 200 HTTP code
     def test_http_success(self):
-        escalator_response = app.test_client().get('/incidents/escalators').status_code
-        # assert that the response code of 'incidents/escalators returns a 200 code
+        escalator_response = (app.test_client().
+                              get('/incidents/escalators').status_code)
+        # Assert that the response code of 'incidents/escalators'
+        # returns a 200 code
         self.assertEqual(escalator_response, 200)
 
-        elevator_response = app.test_client().get('/incidents/elevators').status_code
-        # assert that the response code of 'incidents/elevators returns a 200 code
+        elevator_response = (app.test_client().
+                             get('/incidents/elevators').status_code)
+        # assert that the response code of 'incidents/elevators
+        # returns a 200 code
         self.assertEqual(elevator_response, 200)
 
-################################################################################
+#######################################################################
 
-    # ensure all returned incidents have the 4 required fields
+    # Ensure all returned incidents have the 4 required fields.
     def test_required_fields(self):
-        required_fields = ["StationCode", "StationName", "UnitType", "UnitName"]
+        required_fields = ["StationCode", "StationName", "UnitType",
+                           "UnitName"]
 
-        response_escalators = app.test_client().get('/incidents/escalators')
-        json_response_escalators = json.loads(response_escalators.data.decode())
+        # Parse out escalator incidents.
+        response_escalators = (app.test_client().
+                               get('/incidents/escalators'))
+        json_response_escalators = json.loads(response_escalators.
+                                              data.decode())
+        # Parse out elevator incidents.
+        response_elevators = (app.test_client().
+                              get('/incidents/elevators'))
+        json_response_elevators = (json.loads
+                                   (response_elevators.data.decode()))
 
-        response_elevators = app.test_client().get('/incidents/elevators')
-        json_response_elevators = json.loads(response_elevators.data.decode())
-
-        # for each incident in the JSON response assert that each of the required fields
-        # are present in the response
+        # For each incident in the JSON response assert that each of
+        # the required fields are present in the response.
+        #
+        # Use a nested for loop to check if each field is in each
+        # dictionary. Do this for both elevator and escalator
+        # incidents.
         for item in json_response_escalators:
             for field in required_fields:
                 self.assertIn(field, item)
@@ -35,29 +50,33 @@ class WMATATest(unittest.TestCase):
             for field in required_fields:
                 self.assertIn(field, item)
 
-################################################################################
+#######################################################################
 
-    # ensure all entries returned by the /escalators endpoint have a UnitType of "ESCALATOR"
+    # Ensure all entries returned by the escalators endpoint have a
+    # UnitType of "ESCALATOR"
     def test_escalators(self):
         response = app.test_client().get('/incidents/escalators')
         json_response = json.loads(response.data.decode())
 
-        # for each incident in the JSON response, assert that the 'UnitType' is "ESCALATOR"
+        # For each incident in the JSON response, assert that the
+        # 'UnitType' is "ESCALATOR"
         for item in json_response:
             self.assertEqual(item["UnitType"], "ESCALATOR")
 
-################################################################################
+#######################################################################
 
-    # ensure all entries returned by the /elevators endpoint have a UnitType of "ELEVATOR"
+    # Ensure all entries returned by the elevators endpoint have a
+    # UnitType of "ELEVATOR"
     def test_elevators(self):
         response = app.test_client().get('/incidents/elevators')
         json_response = json.loads(response.data.decode())
 
-        # for each incident in the JSON response, assert that the 'UnitType' is "ELEVATOR"
+        # For each incident in the JSON response, assert that the
+        # 'UnitType' is "ELEVATOR"
         for item in json_response:
             self.assertEqual(item["UnitType"], "ELEVATOR")
 
-################################################################################
+#######################################################################
 
 if __name__ == "__main__":
     unittest.main()
--- wmata_api.py
+++ wmata_api.py
@@ -7,37 +7,51 @@ WMATA_API_KEY = "cff72774ee904e95b645b049dc5a18c4"
 INCIDENTS_URL = "https://api.wmata.com/Incidents.svc/json/ElevatorIncidents"
 headers = {"api_key": WMATA_API_KEY, 'Accept': '*/*'}
 
-################################################################################
+########################################################################
 
 app = Flask(__name__)
 
-# get incidents by machine type (elevators/escalators)
-# field is called "unit_type" in WMATA API response
+# Get incidents by machine type (elevators/escalators)
+# Field is called "unit_type" in WMATA API response
 @app.route("/incidents/<unit_type>", methods=["GET"])
+
+
+# This function will return a json string.
 def get_incidents(unit_type) -> str:
     # create an empty list called 'incidents'
     incidents = []
 
-    # use 'requests' to do a GET request to the WMATA Incidents API
-    # retrieve the JSON from the response
+    # Sse 'requests' to do a GET request to the WMATA Incidents API
+    # Retrieve the JSON from the response
     incident_request = requests.get(INCIDENTS_URL, headers=headers)
     incident_request_json = incident_request.json()
-    incident_dictionaries = incident_request_json.get("ElevatorIncidents")
+    incident_dictionaries = (incident_request_json.get
+                             ("ElevatorIncidents"))
 
-
-    # iterate through the JSON response and retrieve all incidents matching 'unit_type'
-    # for each incident, create a dictionary containing the 4 fields from the Module 7 API definition
-    #   -StationCode, StationName, UnitType, UnitName
-    # add each incident dictionary object to the 'incidents' list
-    # return the list of incident dictionaries using json.dumps()
-
+    # Iterate through the JSON response and retrieve all incidents
+    # matching 'unit_type'. As per the assignment instructions,
+    # 'unit_type' will be elevators and escalators.
+    #
+    # For each incident, create a dictionary containing the 4 fields
+    # from the Module 7 API definition
+    #
+    # The 4 fields are: StationCode, StationName, UnitType, UnitName
+    # Add each incident dictionary object to the 'incidents' list
+    # Return the list of incident dictionaries using json.dumps()
     for item in incident_dictionaries:
         if unit_type == "elevators" and item["UnitType"] == "ELEVATOR":
-            incidents.append({"StationCode" : item.get("StationCode"), "StationName" : item.get("StationName"), "UnitType" : item.get("UnitType"), "UnitName" : item.get("UnitName")})
+            incidents.append({"StationCode" : item.get("StationCode"),
+                              "StationName" : item.get("StationName"),
+                              "UnitType" : item.get("UnitType"),
+                              "UnitName" : item.get("UnitName")})
 
-        elif unit_type == "escalators" and item["UnitType"] == "ESCALATOR":
-            incidents.append({"StationCode" : item.get("StationCode"), "StationName" : item.get("StationName"), "UnitType" : item.get("UnitType"), "UnitName" : item.get("UnitName")})
-
+        elif (unit_type == "escalators" and item["UnitType"] ==
+              "ESCALATOR"):
+            incidents.append({"StationCode" : item.get("StationCode"),
+                              "StationName" : item.get("StationName"),
+                              "UnitType" : item.get("UnitType"),
+                              "UnitName" : item.get("UnitName")})
+        # Skip all 'unit_type" that are not escaltors or elevators.
         else:
             continue
 
